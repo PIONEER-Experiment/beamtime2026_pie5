30dcb388b451db402592620d0a89907a010d9566
Patrick Schwendimann <schwenpa@uw.edu>
2026-07-24 10:10:40 +0200

    [Refactor] Sequencer now relies on ODB and callbacks in daemons rather than calling functions directly

--- sequencer/sequencer.py
+++ sequencer/sequencer.py
@@ -1,38 +1,35 @@
 from midas.sequencer import SequenceClient
+import midas
 
 from pioneer.rundb.interface import interface
 db_interface = interface(user = "bot", password = "bot")
 
 def load_config_to_odb(seq : SequenceClient):
-    aConfig = db_interface.find_next_config()
+    aConfig = db_interface.find_next_run_config()
     if aConfig is None:
-        return None
+        return False
     seq.sequencer_msg(f"Loaded configuration {aConfig.__repr__()}")
-    return aConfig['job_id']
+    seq.odb_set("/Runinfo/Run DB PK", int(aConfig['job_id']))
+    return True
 
-def execute_run(seq : SequenceClient, job_id : int):
+def execute_run(seq : SequenceClient):
     numOfEvents = seq.get_param("nEv")
     seq.start_run()
     seq.wait_seconds(numOfEvents)
     seq.stop_run()
-    run_id = seq.odb_get("/Runinfo/Run number")
-    ret_val = db_interface.end_of_midas_run(job_id, run_id)
-    return ret_val
+    return True
 
 def define_params(seq : SequenceClient):
     seq.register_param("nEv", "Number of Events", 15)
 
 def sequence(seq: SequenceClient):
     while True:
-        try:
-            job_id = load_config_to_odb(seq)
-            if job_id is None:
-                seq.wait_seconds(5)
-                continue
-        except Exception as e:
-            seq.msg(f"An exception occured: {e}")
-            break
-        run_successful = execute_run(seq, job_id)
+        seq.wait_odb("/Runinfo/State", "==", midas.STATE_STOPPED)
+        loaded = load_config_to_odb(seq)
+        if not loaded:
+            seq.wait_seconds(5)
+            continue
+        run_successful = execute_run(seq)
         if not run_successful:
             break
 
