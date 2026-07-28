89dbcebb3bc58ecaa41ae75b9fb02d0a49aa6432
Patrick Schwendimann <schwenpa@uw.edu>
2026-07-28 13:20:59 +0200

    [Documentation] Add comments about sequencer code that needs to be replaced between testing and production operation

--- sequencer/sequencer.py
+++ sequencer/sequencer.py
@@ -8,6 +8,9 @@ def load_config_to_odb(seq : SequenceClient):
     aConfig = db_interface.find_next_run_config()
     if aConfig is None:
         return False
+    # Add code here that pushes each configuration to the ODB.
+    # it might be reasonable to outsource this to dedicated python
+    # modules that get imported
     seq.sequencer_msg(f"Loaded configuration {aConfig.__repr__()}")
     seq.odb_set("/Runinfo/Run DB PK", int(aConfig['job_id']))
     return True
@@ -15,6 +18,10 @@ def load_config_to_odb(seq : SequenceClient):
 def execute_run(seq : SequenceClient):
     numOfEvents = seq.get_param("nEv")
     seq.start_run()
+    # This is where the actual run happens.
+    # The wait_seconds needs to be replaced by a more reasonable
+    # wait until completion logic, e.g. total number of events
+    # sent by a specific frontend or some integrated beam quantity.
     seq.wait_seconds(numOfEvents)
     seq.stop_run()
     return True
